2c795fa380ad1848c5a2a73bf9ece8dea68acbe5
C. Chris Meystrik <meystrik@meystrik.com>
2020-04-28 19:30:09 -0400

    Initial Commit

---
+++ matestHarness/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  matestHarness
+//
+//  Created by Meystrik, Chris on 4/28/20.
+//  Copyright © 2020 C. Chris Meystrik. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ matestHarness/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  matestHarness
+//
+//  Created by Meystrik, Chris on 4/28/20.
+//  Copyright © 2020 C. Chris Meystrik. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
