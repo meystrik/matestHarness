7d4bd22aedcb47df3e4f21ff46e749bf7eb7309c
C. Chris Meystrik <meystrik@meystrik.com>
2022-12-21 20:52:19 -0500

    Main initial edits to the interface and code.

--- matestHarness/ViewController.swift
+++ matestHarness/ViewController.swift
@@ -10,12 +10,44 @@ import Cocoa
 
 class ViewController: NSViewController {
 
+    var mc = MAMouseControl()
+    var retval: Int = 0
+    
+    
+    
+    
+    @IBOutlet weak var xCoordFrom: NSTextField!
+    @IBOutlet weak var yCoordFrom: NSTextField!
+    @IBOutlet weak var xCoordTo: NSTextField!
+    @IBOutlet weak var yCoordTo: NSTextField!
+    
+    @IBOutlet weak var moveToLabel: NSTextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func moveToCoord(_ sender: Any) {
+        print("Button pushed for movetoCoord\n")
+        moveToLabel.stringValue = "Moving from \(xCoordFrom.stringValue) : \(yCoordFrom.stringValue) to    \(xCoordTo.stringValue) : \(yCoordTo.stringValue)"
+        retval = Int(mc.move(toCoordinates: xCoordFrom.intValue, andPointY: yCoordFrom.intValue))
+    }
+    
+    @IBAction func smoothMove(_ sender: Any) {
+        print("Button pushed for smoothMove\n")
+        moveToLabel.stringValue = "Moving from \(xCoordFrom.stringValue) : \(yCoordFrom.stringValue) to    \(xCoordTo.stringValue) : \(yCoordTo.stringValue)"
+        retval = Int(mc.moveSmoothBetweenPoints(xCoordFrom.intValue, andPointY: yCoordFrom.intValue, x2: xCoordTo.intValue, andPointY2: yCoordTo.intValue))
 
+    }
+    
+    @IBAction func coodTyping(_ sender: Any) {
+        moveToLabel.stringValue = "Moving from \(xCoordFrom.stringValue) : \(yCoordFrom.stringValue) to    \(xCoordTo.stringValue) : \(yCoordTo.stringValue)"
+    }
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
